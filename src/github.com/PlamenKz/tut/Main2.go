34f1d5d36917459865411f2a3d79f0263231497e
Plamen <pkzumanov0@gmail.com>
2021-09-07 23:12:57 +0200

    k

--- src/github.com/PlamenKz/tut/Main2.go
+++ src/github.com/PlamenKz/tut/Main2.go
@@ -3,9 +3,26 @@ package main
 import "fmt"
 
 //array
+//slicing operations
+// a[:] all elements
+// a[3:] from 4th elem to end
+// a[:6] first 6 items
+// a[3:6] from 4th to 6th (4,5,6)
+
+//a := []int{} -> slice
 
 func main() {
+	//3x3 array
+	var threes [3][3]float32
+	//go copies array if assinged
+	b := threes
+	//reference
+	c := &threes
 	fmt.Println("hello world")
 	grades := [3]int{97, 85, 83}
-	fmt.Printf("Grades %v", grades)
+	grades[0] = 7
+	fmt.Printf("Grades %v \n", grades)
+	fmt.Printf("%v", threes)
+	fmt.Printf("%v", b)
+	fmt.Printf("%v", c)
 }
